84bbda2cbcb431dc1a530ac4fef7175ed100e720
Marek Kovacik <manikoske@gmail.com>
2017-03-14 00:10:02 +0100

    initial

---
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package com.mygdx.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.game.WordCloud;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 1000;
+		config.height = 1000;
+		new LwjglApplication(new WordCloud(), config);
+	}
+}
